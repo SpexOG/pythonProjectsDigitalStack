d7406d122ce876bb35783ee00090428e77b2059c
Sebastian Mutulescu <sebastian.mutulescu9@gmail.com>
2025-09-24 21:33:24 +0300

    variabile

---
+++ Curs1/Variabile.py
@@ -0,0 +1,62 @@
+# var = 10
+# print(var, type(var), id(var))
+
+# x = [1, 2, 3]
+# y = x
+# y.append(4)
+# print(x, y)
+#
+# a = [1, 2, 3]
+# b = a
+# del a
+# print(b)
+#
+# name = "Ana"
+# print(name)
+# my_name = "Sebastian"
+# print(my_name)
+#
+# _is_active = True
+# print(_is_active)
+# _is_active = False
+# print(_is_active)
+#
+# value = 10
+# value = 'abc'
+# value = [1, 2, 3]
+#
+# a, b, c = 1, 2, 3
+# x = y = z = 10
+
+# Operatori aritmetici:
+## +, -, *, /, //, %, **
+
+# var1 = 5 //2
+# print(var1)
+
+# Operatori de comparatie: (returneaza boolean adica True sau False)
+# ==, !=, >, <. >=, <=
+# print (2 != 2)
+
+# Operatori logici:
+# and, or, not
+
+# Operatori de atribuire
+# = atribuire
+# x = 5
+# x = x + 5
+# x += 5
+# print(x)
+
+# Operatori de identitate
+# is, is not
+
+# Operatori de apartenenta
+# in, not in
+# print(4 in [1,2,3])
+
+var3 = 2 + 5j
+var2 = 7 - 3j
+var1 = var2 + var3
+print(var3, type(var3))
+print(var1, type(var1))
